9565edb36fecf1327ba795fc50f18d882bfe956a
Umesh Parulekar <ucparule@us.ibm.com>
2017-06-28 11:11:14 -0500

    sq

---
+++ src/main/java/testsonarqube/App.java
@@ -0,0 +1,16 @@
+package testsonarqube;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Invoking FreqTable!" );
+        String arguments[] = {};
+        (new FreqTable()).run(arguments);
+    }
+}
